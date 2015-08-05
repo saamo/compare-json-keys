145c95b194866332332c8302ffc8a4e9c1922151
Samir Djellil <samir@djellil.com>
2015-08-05 14:56:00 +0200

    Fix typo

--- test.js
+++ test.js
@@ -2,6 +2,6 @@ var assert = require('assert');
 var compareJsonKeys = require('./');
 
 it('should compare keys', function() {
-  assert.equal(compareJsonKeys({ a: 1, b: 2 }, { b: 1, a: 2 }), true, 'it should have returned true because keys are same');
-  assert.notEqual(compareJsonKeys({ a: 1, b: 2 }, { b: 1, c: 2 }), true, 'it should have returned false because keys are not same');
+  assert.equal(compareJsonKeys({ a: 1, b: 2 }, { b: 1, a: 2 }), true, 'it should have returned true because keys are the same');
+  assert.notEqual(compareJsonKeys({ a: 1, b: 2 }, { b: 1, c: 2 }), true, 'it should have returned false because keys are not the same');
 });
